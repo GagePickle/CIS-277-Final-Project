f1e845966b739d1ac5e9800a520b606f7a70799c
Gage Pickle <gagepickle@yahoo.com>
2020-12-07 17:11:31 -0600

    Update html for form

--- components/Main/Form.js
+++ components/Main/Form.js
@@ -1,23 +1,26 @@
-export default () => `<form>
-  <span>
-    <label for="name">Item Name</label>
-    <input type="text" id="name" placeholder="Item Name" required />
-  </span>
+export default () => `
+<div>
+  <form>
+    <span>
+      <label for="name">Item Name</label>
+      <input type="text" id="name" placeholder="Item Name" required />
+    </span>
 
-  <span>
-    <label for="price">Item Price</label>
-    <input
-    type="number"
-    id="price"
-    min="0"
-    placeholder="Item Price"
-    required
-  </span>
+    <span>
+      <label for="price">Item Price</label>
+      <input
+      type="number"
+      id="price"
+      min="0"
+      placeholder="Item Price"
+      required
+    </span>
 
-  <span>
-    <label for="info">Additional Info.</label>
-    <input type="text" id="info" placeholder="Additional Info" />
-  </span>
-  <button type="submit">Add Item</button>
-</form>
+    <span>
+      <label for="info">Additional Info.</label>
+      <input type="text" id="info" placeholder="Additional Info" />
+    </span>
+    <button type="submit">Add Item</button>
+  </form>
+</div>
 `;
