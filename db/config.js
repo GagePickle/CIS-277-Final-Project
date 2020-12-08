0f1c5c6259bf5be5cef1681e913bc92bfcdb679b
Gage Pickle <gagepickle@yahoo.com>
2020-12-07 18:14:42 -0600

    Connect firebase to the app

---
+++ db/config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyCJbptbGxpnGddY8BiEfw7zm3fTkiZkypw",
+  authDomain: "eft-item-tracker.firebaseapp.com",
+  projectId: "eft-item-tracker",
+  storageBucket: "eft-item-tracker.appspot.com",
+  messagingSenderId: "443573623675",
+  appId: "1:443573623675:web:d251eef6a912f2f6d23bde",
+};
