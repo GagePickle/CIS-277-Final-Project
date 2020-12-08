3b7d04f586b77ea31bb751a8aac945edd72cd134
Gage Pickle <gagepickle@yahoo.com>
2020-12-07 19:17:30 -0600

    Add 'name' attribute to form inputs

--- components/Main/Form.js
+++ components/Main/Form.js
@@ -3,7 +3,7 @@ export default () => `
   <form>
     <span>
       <label for="name">Item Name</label>
-      <input type="text" id="name" placeholder="Item Name" required />
+      <input type="text" id="name" placeholder="Item Name" name="name" required />
     </span>
 
     <span>
@@ -13,12 +13,13 @@ export default () => `
       id="price"
       min="0"
       placeholder="Item Price"
+      name="price"
       required
     </span>
 
     <span>
       <label for="info">Additional Info.</label>
-      <input type="text" id="info" placeholder="Additional Info" />
+      <input type="text" id="info" placeholder="Additional Info" name="info" />
     </span>
     <button type="submit">Add Item</button>
   </form>
