a3e2fa01da9a9388600ff066616ff10967acdfcf
Manav Misra <manavm@visionwebsoft.com>
2020-12-10 18:59:23 -0600

    :poop: Add demo code for toggling spans via CSS

--- components/Main/Table/THead.js
+++ components/Main/Table/THead.js
@@ -5,15 +5,15 @@ export default () => `
         Item Name
         <!-- Use CSS to give adequate padding and 'click area' -->
         <button data-prop="itemName">
-        <span>🔻</span>
+        <span></span>
         </button>
       </td>
       <td>
         Item Price<button></button>
       </td>
       <td>Additional Information
-      <button data-prop="additionalInfo">
-        <span>🔻</span>
+      <button data-prop="additionalInfo" >
+        <span data-order="desc"></span>
         </button></td>
     </tr>
   </thead>
